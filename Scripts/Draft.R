ad36e4bee7497721029bc8f89cde3594e68c1d1b
Zordo <pedrogodinhoc@gmail.com>
2019-03-30 18:55:37 +0100

    5thCommit

--- Scripts/Draft.R
+++ Scripts/Draft.R
@@ -3,15 +3,15 @@ library(readr)
 library(rstudioapi)
 library(e1071)
 library(dplyr)
+library(rpart)
 
 
-
+#rm(list = setdiff(ls(), lsf.str()))
 
 current_path = getActiveDocumentContext()$path
 setwd(dirname(current_path))
 setwd("..")
 rm(current_path)
-#rm(list = setdiff(ls(), lsf.str()))
 EP <- read.csv( file ="./data/Epa.csv" , header = TRUE , sep = ',')
 NP <- read.csv(file = "./data/Npa.csv", header = TRUE , sep =',')
 EP <- EP[,c(1,5,9,18)]
@@ -31,9 +31,9 @@ Knn <- RmOut(Knn)
 
 EP <- RmOut(EP)
 
-List <- TrainAndTestSets(EP$Volume,0.75,EP,123)
+List <- TrainAndTestSets(EP$Volume,0.75,EP,233)
 
-ListK <- TrainAndTestSets(Knn$Volume,0.75,Knn,123)
+ListK <- TrainAndTestSets(Knn$Volume,0.75,Knn,233)
 
 
 ModelRandomForest <- TrainingFunction(Volume ~.,List$trainingSet,"rf",5)
@@ -44,21 +44,28 @@ KNN <- TrainingFunction(Volume ~.,ListK$trainingSet,"knn",5)
 
 PredictionRandomForest <- predict(ModelRandomForest,List$testingSet)
 
- postResample(PredictionRandomForest,List$testingSet$Volume)
+TestResultsRF <- postResample(PredictionRandomForest,List$testingSet$Volume)
 
-KnnPrediction <- predict(KNN,testingsetknn)
+KnnPrediction <- predict(KNN,ListK$testingSet)
 
-testingsetknn <- ListK$testingSet
-
-postResample(KnnPrediction,testingsetknn$Volume)
-
-PredictionRandomForest <-  as.data.frame(PredictionRandomForest)
+TestResultsKNN <-postResample(KnnPrediction,ListK$testingSet$Volume)
 
 
 
+SVM <- train()
 
 
+svm.model <- svm(V4 ~ ., data = trainset, cost = 1000, gamma = 0.0001)
+svm.pred  <- predict(svm.model, testset[,-3])
+  crossprod(svm.pred - testset[,3]) / length(testindex)
 
 
+svm.model <- svm(Volume ~ . , data = List$trainingSet,cost=1000 , gamma = 0.001)
 
-                         
+svm.pred <- predict(svm.model,List$testingSet)
+
+TestResultsSVM <- postResample(svm.pred,List$testingSet$Volume)
+
+AllTestResults <- cbind(TestResultsKNN,TestResultsRF,TestResultsSVM)
+
+AllTestResults                         
